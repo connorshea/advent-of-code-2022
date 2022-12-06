6c6f7359a0f29b4fba89ac61be8d18ca789abb40
Connor Shea <connor.james.shea@gmail.com>
2022-12-05 19:32:12 -0700

    Refactor a bit for Day 5 Part 2

--- day5.rb
+++ day5.rb
@@ -1,22 +1,29 @@
 require 'debug'
-lines = File.readlines('day5input.txt')
-# lines = File.readlines('day5testinput.txt')
-lines.map! { |line| line.delete("\n") }
 
-breaking_line = lines.find_index { |line| line == '' }
+def get_input_lines
+  lines = File.readlines('day5input.txt')
+  # lines = File.readlines('day5testinput.txt')
+  lines.map! { |line| line.delete("\n") }
 
-graphic_lines = lines[0...breaking_line - 1]
-move_lines = lines[(breaking_line + 1)..]
+  breaking_line = lines.find_index { |line| line == '' }
 
-graphic_lines.map! { |line| line.gsub('    ', '~').gsub(' ', '').gsub(/\[|\]/, '') }
+  graphic_lines = lines[0...breaking_line - 1]
+  mv_lines = lines[(breaking_line + 1)..]
 
-# Turn each line into an array of elements, where the first item is the bottom
-# of each stack, and then yeet the empty spaces.
-@rotated_graphic_lines = graphic_lines.map { |line| line.split('') }.transpose.map(&:reverse).map do |line|
-  line.map { |el| el == '~' ? nil : el }.compact
+  graphic_lines.map! { |line| line.gsub('    ', '~').gsub(' ', '').gsub(/\[|\]/, '') }
+
+  # Turn each line into an array of elements, where the first item is the bottom
+  # of each stack, and then yeet the empty spaces.
+  rotated_lines = graphic_lines.map { |line| line.split('') }.transpose.map(&:reverse).map do |line|
+    line.map { |el| el == '~' ? nil : el }.compact
+  end
+
+  return rotated_lines, mv_lines
 end
 
-move_lines.map! do |line|
+@rotated_graphic_lines, @move_lines = *get_input_lines
+
+@move_lines.map! do |line|
   count, from, to = line.match(/move (\d+) from (\d+) to (\d+)/).to_a[1..].map(&:to_i)
 
   {
@@ -26,7 +33,7 @@ move_lines.map! do |line|
   }
 end
 
-move_lines.each do |move_hash|
+@move_lines.each do |move_hash|
   move_hash[:count].times do
     to = move_hash[:to]
     from = move_hash[:from]
@@ -35,3 +42,15 @@ move_lines.each do |move_hash|
 end
 
 puts @rotated_graphic_lines.map { |line| line.last }.join('')
+
+### Part 2 ###
+
+@rotated_graphic_lines_for_part2, _ = *get_input_lines
+
+@move_lines.each do |move_hash|
+  to = move_hash[:to]
+  from = move_hash[:from]
+  @rotated_graphic_lines_for_part2[to].push(*@rotated_graphic_lines_for_part2[from].pop(move_hash[:count]))
+end
+
+puts @rotated_graphic_lines_for_part2.map { |line| line.last }.join('')
